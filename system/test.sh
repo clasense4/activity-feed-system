ce355394095febaa03e1ad91f8a581217afcb79e
Fajri Abdillah <clasense4@gmail.com>
2019-02-08 00:12:07 +0700

    Refresh migration before test

--- system/test.sh
+++ system/test.sh
@@ -1,3 +1,6 @@
+cd ..
+orator migrate:refresh -c system/config/database.yml --force --seed
+cd system
 rm -rf cover
 rm -rf .coverage
 nosetests tests/test_* --with-coverage --cover-package=main,decorators,controllers --logging-level=DEBUG --no-byte-compile --cover-html
