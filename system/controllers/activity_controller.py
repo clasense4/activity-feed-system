e41023eea7059225f1c998e01f9c07becab6a4ce
Fajri Abdillah <clasense4@gmail.com>
2019-02-09 17:03:52 +0700

    Remove verb post

--- system/controllers/activity_controller.py
+++ system/controllers/activity_controller.py
@@ -24,7 +24,7 @@ class controller(base_controller.controller):
 
             # Validate verb (like, post, share)
             #   "verb":"like"
-            valid_verb = ['like', 'post', 'share', 'follow']
+            valid_verb = ['like', 'share']
             verb = request.json['verb']
             if str.lower(verb) in valid_verb:
                 params['verb'] = verb
@@ -34,44 +34,6 @@ class controller(base_controller.controller):
                     "message": "Wrong verb format"
                 }
                 return content, 400
-
-            # Validation for post
-            if verb == 'post':
-                # Must have object
-                object_params = self.valid_object_format(request)
-                if object_params == False:
-                    content = {
-                        "error": True,
-                        "message": "Wrong object format"
-                    }
-                    return content, 400
-
-                # Create record in post/photo table
-                """
-                TODO : Add 'content' in verb 'post' including its validation
-                post => Raw Text
-                photo => Image URL
-                """
-                post_verb_table_mapping = {
-                    'post': 'posts',
-                    'photo': 'photos'
-                }
-                object_name = request.json['object'].split(":")[0]
-                db.table(post_verb_table_mapping[object_name]).insert({
-                    'user_id': params['actor_id'],
-                    'content': "Foobar",
-                    'created_at': 'now()',
-                    'updated_at': 'now()'
-                })
-
-                activity_params = self.merge_two_dicts(params, object_params)
-                db.table('activities').insert(activity_params)
-
-                content = {
-                    "verb": verb,
-                    "message": "Activity recorded"
-                }
-                return content, 200
 
             # Validation for like
             if verb in ['like', 'share']:
